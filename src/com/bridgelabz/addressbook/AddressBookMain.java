7532c61da37a925fcde7871ac4b04e1999387bd5
mk3709 <mk3709@srmist.edu.in>
2024-01-31 17:58:12 +0530

    MultipleContactsadded

--- src/com/bridgelabz/addressbook/AddressBookMain.java
+++ src/com/bridgelabz/addressbook/AddressBookMain.java
@@ -3,25 +3,78 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
-
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
 class ContactPerson {
-    private String name;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String state;
+    private String zip;
     private String phoneNumber;
     private String email;
 
-    public ContactPerson(String name, String phoneNumber, String email) {
-        this.name = name;
+    public ContactPerson(String firstName, String lastName, String address, String city, String state, String zip, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
         this.phoneNumber = phoneNumber;
         this.email = email;
     }
 
-    public String getName() {
-        return name;
+    public String getFirstName() {
+        return firstName;
     }
 
-    public void setName(String name) {
-        this.name = name;
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
     }
 
     public String getPhoneNumber() {
@@ -42,7 +95,13 @@ class ContactPerson {
 
     @Override
     public String toString() {
-        return "Name: " + name + ", Phone: " + phoneNumber + ", Email: " + email;
+        return "Name: " + firstName + " " + lastName +
+                "\nAddress: " + address +
+                "\nCity: " + city +
+                "\nState: " + state +
+                "\nZip: " + zip +
+                "\nPhone Number: " + phoneNumber +
+                "\nEmail: " + email;
     }
 }
 
@@ -60,12 +119,13 @@ class AddressBook {
     public void displayContacts() {
         for (ContactPerson contact : contacts) {
             System.out.println(contact);
+            System.out.println("----------------------");
         }
     }
 
     public ContactPerson findContactByName(String name) {
         for (ContactPerson contact : contacts) {
-            if (contact.getName().equalsIgnoreCase(name)) {
+            if ((contact.getFirstName() + " " + contact.getLastName()).equalsIgnoreCase(name)) {
                 return contact;
             }
         }
@@ -87,7 +147,7 @@ class AddressBook {
         Iterator<ContactPerson> iterator = contacts.iterator();
         while (iterator.hasNext()) {
             ContactPerson contact = iterator.next();
-            if (contact.getName().equalsIgnoreCase(name)) {
+            if ((contact.getFirstName() + " " + contact.getLastName()).equalsIgnoreCase(name)) {
                 iterator.remove();
                 System.out.println("Contact deleted successfully.");
                 return;
@@ -118,14 +178,24 @@ public class AddressBookMain {
             switch (choice) {
                 case 1:
                     System.out.println("\nEnter details for a new contact:");
-                    System.out.print("Name: ");
-                    String newName = scanner.nextLine();
+                    System.out.print("First Name: ");
+                    String firstName = scanner.nextLine();
+                    System.out.print("Last Name: ");
+                    String lastName = scanner.nextLine();
+                    System.out.print("Address: ");
+                    String address = scanner.nextLine();
+                    System.out.print("City: ");
+                    String city = scanner.nextLine();
+                    System.out.print("State: ");
+                    String state = scanner.nextLine();
+                    System.out.print("ZIP: ");
+                    String zip = scanner.nextLine();
                     System.out.print("Phone Number: ");
-                    String newPhoneNumber = scanner.nextLine();
+                    String phoneNumber = scanner.nextLine();
                     System.out.print("Email: ");
-                    String newEmail = scanner.nextLine();
+                    String email = scanner.nextLine();
 
-                    ContactPerson newContact = new ContactPerson(newName, newPhoneNumber, newEmail);
+                    ContactPerson newContact = new ContactPerson(firstName, lastName, address, city, state, zip, phoneNumber, email);
                     addressBook.addContact(newContact);
                     System.out.println("Contact added successfully.");
                     break;
@@ -142,11 +212,11 @@ public class AddressBookMain {
                     if (existingContactToEdit != null) {
                         System.out.println("\nExisting Contact Details:\n" + existingContactToEdit);
                         System.out.print("\nEnter new phone number: ");
-                        String newPhoneNumberForEdit = scanner.nextLine();
+                        String newPhoneNumber = scanner.nextLine();
                         System.out.print("Enter new email: ");
-                        String newEmailForEdit = scanner.nextLine();
+                        String newEmail = scanner.nextLine();
 
-                        addressBook.editContact(contactNameToEdit, newPhoneNumberForEdit, newEmailForEdit);
+                        addressBook.editContact(contactNameToEdit, newPhoneNumber, newEmail);
                     } else {
                         System.out.println("Contact not found.");
                     }
